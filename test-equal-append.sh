d0ef6188a8f33909c6324b6e97c6bbbd661dc697
Rijnard van Tonder <rvantonder@gmail.com>
2018-11-01 20:52:44 +0000

    ejabberd

---
+++ test-equal-append.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd ejabberd-erl-tidy-workspace
+git checkout -- .
+time erl -noshell -eval 'erl_tidy:dir("",[{keep_unused, true},{backups, false},{auto_list_comp, true}]).' -s init stop &> /dev/null
+cd ..
+
+cd ejabberd-teresy-workspace
+git checkout -- .
+time ~/rooibos-future/main -d . -filter .erl -templates ~/rooibos-future/catalogue/erlang/tidier/append-4.3
+cd ..
+
+EQUAL=`diff -y --suppress-common-lines ejabberd-{erl-tidy,teresy}-workspace/mod_pubsub.erl`
+
+echo "Diffs" ${EQUAL}
+
+cd ejabberd-erl-tidy-workspace
+git checkout -- .
+
+cd ejabberd-teresy-workspace
+git checkout -- .
+
